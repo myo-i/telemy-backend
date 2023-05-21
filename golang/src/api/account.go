6063f9428eb006073d6aec8de65348ea3e976747
myo-i <Imiyo0521@gmail.com>
2023-05-22 03:43:42 +0900

    🚧 getAccount(ハンドラーファンク)の作成

--- golang/src/api/account.go
+++ golang/src/api/account.go
@@ -5,7 +5,26 @@ import (
 	"net/http"
 )
 
-func account(w http.ResponseWriter) {
+func createAccount(w http.ResponseWriter) {
 	str := "test"
 	fmt.Fprintf(w, "create account: %s", str)
+}
+
+func (server *Server) getAccount(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	userID := 1
+	url := fmt.Sprintf("https://localhost/accounts/%d", userID)
+
+	account, err := server.querier.GetAccount(id)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		if resp.StatusCode == http.StatusNotFound {
+			http.Error(w, "指定したIDを持つデータが存在しません", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "リクエストの送信に失敗しました", http.StatusInternalServerError)
+		return
+	}
+
 }
