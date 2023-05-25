ebd0ec11a4e4d2f359e24e3885e8f2b5c622d666
myo-i <Imiyo0521@gmail.com>
2023-05-26 03:01:45 +0900

    🚧 とりあえずDBからデータ取ってくる

--- golang/src/api/account.go
+++ golang/src/api/account.go
@@ -2,7 +2,9 @@ package api
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"telemy/db"
 )
 
 func createAccount(w http.ResponseWriter) {
@@ -11,11 +13,9 @@ func createAccount(w http.ResponseWriter) {
 }
 
 func (server *Server) getAccount(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-	userID := 1
-	url := fmt.Sprintf("https://localhost/accounts/%d", userID)
-
-	account, err := server.querier.GetAccount(id)
+	// id := r.URL.Query().Get("id")
+	id := "1"
+	url := fmt.Sprintf("https://localhost/accounts/%d", id)
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -26,5 +26,31 @@ func (server *Server) getAccount(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "リクエストの送信に失敗しました", http.StatusInternalServerError)
 		return
 	}
+	account := server.queries.GetAccount(id)
+	fmt.Fprintf(w, "ID:%v, Nickname:%s, Email:%s, Password:%s, Created_at:%s", &account.ID, account.Nickname, account.Email, account.Password, account.Created_at)
+
+}
+
+func echoHello(w http.ResponseWriter, r *http.Request) {
+	dbConnection := db.ConnectDB()
+	defer dbConnection.Close()
+	fmt.Fprintf(w, "<h1>Hello World</h1>\n<h2>Hello World</h2>\n<h3>Hello World</h3>")
+
+	rows, err := dbConnection.Query("SELECT * FROM accounts WHERE id = 1 LIMIT 1;")
+	defer rows.Close()
+	if err != nil {
+		log.Fatalf("Failed to select: %s", err)
+	}
+
+	for rows.Next() {
+		var account db.Account
+		err := rows.Scan(&account.ID, &account.Nickname, &account.Email, &account.Password, &account.Created_at)
+
+		if err != nil {
+			log.Fatalf("Failed to get rows: %s", err)
+		}
+		fmt.Fprintf(w, "ID:%v, Nickname:%s, Email:%s, Password:%s, Created_at:%s", &account.ID, account.Nickname, account.Email, account.Password, account.Created_at)
+	}
+	// fmt.Fprintf(w, "DB resule: %s", a)
 
 }
--- golang/src/api/server.go
+++ golang/src/api/server.go
@@ -1,14 +1,12 @@
 package api
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 	"telemy/db"
 )
 
 type Server struct {
-	querier db.Querier
+	queries db.Queries
 }
 
 func NewServer() {
@@ -18,45 +16,15 @@ func NewServer() {
 	queries := db.NewQueries(dbConnection)
 
 	server := Server{
-		querier: queries,
+		queries: queries,
 	}
 
-	server.setupRouter(dbConnection)
+	server.setupRouter()
 }
 
 func (server *Server) setupRouter() {
 	http.HandleFunc("/", echoHello)
 	http.HandleFunc("/accounts", server.getAccount)
 	http.ListenAndServe(":8080", nil)
-
-}
-
-type Article struct {
-	Id    int
-	Title string
-	Body  string
-}
-
-func echoHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello World</h1>\n<h2>Hello World</h2>\n<h3>Hello World</h3>")
-	a := db.ConnectDB()
-	defer a.Close()
-
-	rows, err := a.Query("SELECT * FROM article;")
-	defer rows.Close()
-	if err != nil {
-		log.Fatalf("Failed to select: %s", err)
-	}
-
-	for rows.Next() {
-		var article Article
-		err := rows.Scan(&article.Id, &article.Title, &article.Body)
-
-		if err != nil {
-			log.Fatalf("Failed to get rows: %s", err)
-		}
-		fmt.Fprintf(w, "ID:%v, Title:%s, Body:%s", article.Id, article.Title, article.Body)
-	}
-	fmt.Fprintf(w, "DB resule: %s", a)
 
 }
