68e69ab759de5ceadac84fca83be343395ab9e17
myo-i <Imiyo0521@gmail.com>
2023-05-26 05:17:32 +0900

    🩹 Accountをjsonに変更

--- golang/src/db/models.go
+++ golang/src/db/models.go
@@ -1,9 +1,9 @@
 package db
 
 type Account struct {
-	ID         int
-	Nickname   string
-	Email      string
-	Password   string
-	Created_at string
+	ID        int    `json:"id"`
+	Nickname  string `json:"nickname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"created_at"`
 }
