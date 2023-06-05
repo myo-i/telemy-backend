48d844ed85dfa1c6ef02116ffc2df281c47541fc
myo-i <Imiyo0521@gmail.com>
2023-06-06 05:16:12 +0900

    🎨 DB関連の汎用メソッドと構造体の移動

--- golang/src/db/accounts.go
+++ golang/src/db/accounts.go
@@ -1,36 +1,9 @@
 package db
 
 import (
-	"database/sql"
-	"fmt"
-	"os"
-
 	// api "command-line-argumentsC:\\Users\\PC_User\\MyProject\\telemy-backend\\golang\\src\\api\\account.go"
 	_ "github.com/go-sql-driver/mysql"
 )
-
-type Queries struct {
-	connection *sql.DB
-}
-
-// TDDでDI使って書く！！！！！！！！！！！！！！！！！！！！！！！
-func ConnectDB() *sql.DB {
-	var path string = fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=true",
-		os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"),
-		os.Getenv("MYSQL_DATABASE"))
-
-	db, err := sql.Open("mysql", path)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	return db
-}
-
-func NewQueries(db *sql.DB) Queries {
-	return Queries{
-		connection: db,
-	}
-}
 
 type CreateAccountRequest struct {
 	Nickname string `json:"nickname"`
